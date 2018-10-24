d20fdbf83c078388aae37139237820dab4e19f77
JosueR98 <joracha98@hotmail.com>
2018-10-24 17:24:19 -0600

    ultimos cambios XD

--- src/activos/application.java
+++ src/activos/application.java
@@ -15,7 +15,7 @@ import activos.loginPresentation.loginView;
  */
 
 public class application {
-    // Ultimos cambios
+    // Ultimos cambios xD
      public static loginModel login_model = new loginModel();
      public static loginView login_view = new loginView();
      public static loginController login_controller= new loginController(login_model,login_view);
